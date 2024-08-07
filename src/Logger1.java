27f85ba06c24872129ecf6594c94093fbd66d4f3
N Sriram <sriramnandula14@gmail.com>
2024-08-07 08:26:53 +0530

    Add files via upload
    Week_2_Inlab

---
+++ src/Logger1.java
@@ -0,0 +1,17 @@
+
+public class Logger1 {
+	private static Logger1 instance;
+	private Logger1(){
+	}
+	
+	public static Logger1 getInstance(){
+	if(instance==null){
+	instance=new Logger1();
+	}
+	return instance;
+	}
+	public void log(String message){
+		
+	System.out.println("Logging message: "+message);
+	}
+}
